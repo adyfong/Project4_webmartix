e458815b851db2bd4849d2af7ba53d13a41c8c68
ady fong <adyfong@gmail.com>
2016-12-13 23:02:51 -0500

    added Forms for React

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router';
 import App from './components/App/App.jsx';
+import SurveyForm from './components/SurveyForm/SurveyForm.jsx';
+import ChartForm from './components/ChartForm/ChartForm.jsx';
 
+// import Home from ''
 // import Chart from './components/Chart/Chart.jsx'
 
 
 
-
-
 // mount our App at #container
-ReactDOM.render(<App />, document.querySelector('#root-container'));
+// ReactDOM.render(<App />, document.querySelector('#root-container'));
 
-// ReactDOM.render((
-//    <Router history={hashHistory}>
-//       <Route path="/" component={App}>
-//         <IndexRoute component={Home}/>
-//         <Route path="/chart" component={Chart}/>
-//       </Route>
-//    </Router>
-//  ), document.querySelector('#root-container'));
+      // <Route path="/" component={App}>
+        // <IndexRoute component={Home}/>
+        // <Route path="/chart" component={Chart}/>
+render((
+   <Router history={hashHistory}>
+      <Route path="/" component={App}/>
+      <Route path="/survey" component={SurveyForm}/>
+      <Route path="/chart" component={ChartForm}/>
+   </Router>
+ ), document.querySelector('#root-container'));
